b29b885d7c96f72185687fd6ce4e9d2bdc0b918d
Kristjan Schröder <kristj@nschroeder.com>
2016-03-16 15:41:11 +0100

    add Calculator.php add add.php github test

---
+++ sources/src/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daedadix
+ * Date: 3/16/2016
+ * Time: 3:40 PM
+ */
+
+namespace HsBremen\WebApi;
+
+
+class Calculator
+{
+
+}
---
+++ sources/web/add.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daedadix
+ * Date: 3/16/2016
+ * Time: 3:37 PM
+ */
+
+require_once
+
+$a = $_GET['a'];
+$b = $_GET['b'];
+
+$calc = new Calculator();
+$result = $calc ->add($a, $b);
+
